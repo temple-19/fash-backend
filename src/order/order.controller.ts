a25d4ffa7b6233041b926c2f596bb737b9a4d67a
temple_19 <tozoaz@gmail.com>
2024-10-11 14:50:15 +0100

    noclgwebhook

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -26,7 +26,7 @@ export class OrderController {
 
   @Post('web')
   async handleWebhook(@Body() body: any) {
-    console.log('Webhook event data:', body.data.reference.event);
+    // console.log('Webhook event data:', body.data.reference);
     //if the body.event = "paymentrequest.success"do this {
     return this.orderService.webhook(body.data.reference);
     //}
